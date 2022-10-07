bf2335ffd3cf76773a7c472afcc6e8857c57a95c
EzyhB <ezyhbmail@gmail.com>
2022-10-07 11:30:32 +0100

    added image slots upto 40

--- db/scripts/createTable.ts
+++ db/scripts/createTable.ts
@@ -8,7 +8,7 @@ const carsTable = () => {
 
 const carImageTable = () => {
   return query(
-    "DROP TABLE IF EXISTS car_images; CREATE TABLE IF NOT EXISTS car_images (id TEXT, image1 TEXT, image2 TEXT, image3 TEXT, image4 TEXT, image5 TEXT, image6 TEXT, image7 TEXT, image8 TEXT, image9 TEXT, image10 TEXT, image11 TEXT, image12 TEXT)"
+    "DROP TABLE IF EXISTS car_images; CREATE TABLE IF NOT EXISTS car_images (id TEXT, image1 TEXT, image2 TEXT, image3 TEXT, image4 TEXT, image5 TEXT, image6 TEXT, image7 TEXT, image8 TEXT, image9 TEXT, image10 TEXT, image11 TEXT, image12 TEXT, image13 TEXT, image14TEXT, image15 TEXT, image16 TEXT, image17 TEXT, image18 TEXT, image19 TEXT, image20 TEXT, image21 TEXT, image22 TEXT, image23 TEXT, image24 TEXT, image25TEXT, image26 TEXT, image27 TEXT, image28 TEXT, image29TEXT, image30 TEXT, image31 TEXT, image32 TEXT, image33 TEXT, image34 TEXT, image35 TEXT, image36 TEXT, image37 TEXT, image38 TEXT, image39 TEXT, image40 TEXT)"
   );
 };
 
